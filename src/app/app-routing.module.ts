79b081789f04f7ad80412c261060bf0095209af6
Nathan Downer <nathandowner123@gmail.com>
2019-06-23 16:02:07 -0500

    added new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  // { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  // { path: 'people', loadChildren: './pages/people/people.module#PeoplePageModule' },
+  // { path: 'planets', loadChildren: './pages/planets/planets.module#PlanetsPageModule' },
+  // { path: 'films', loadChildren: './pages/films/films.module#FilmsPageModule' },
+  // { path: 'film-details', loadChildren: './pages/film-details/film-details.module#FilmDetailsPageModule' }
 ];
 @NgModule({
   imports: [
